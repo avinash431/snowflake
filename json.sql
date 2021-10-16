a9526abb45606bec5f2d38d9992cfca66fac7ae0
Avinash <sreethalam.avinash@gmail.com>
2021-10-17 00:20:35 +0530

    Update json.sql

--- json.sql
+++ json.sql
@@ -1,15 +1,180 @@
-//returns entire record
-select raw_author 
-from author_ingest_json;
+// First step: Load Raw JSON
 
-//returns AUTHOR_UID value from top-level object's attribute
-select raw_author:AUTHOR_UID
-from author_ingest_json;
+CREATE DATABASE MANAGE_DB;
+CREATE SCHEMA MANAGE_DB.EXTERNAL_STAGES;
+CREATE SCHEMA MANAGE_DB.FILE_FORMATS;
+CREATE DATABASE OUR_FIRST_DB;
 
-//returns the data in a way that makes it look like a normalized table
+CREATE OR REPLACE stage MANAGE_DB.EXTERNAL_STAGES.JSONSTAGE
+     url='s3://bucketsnowflake-jsondemo';
+
+CREATE OR REPLACE file format MANAGE_DB.FILE_FORMATS.JSONFORMAT
+    TYPE = JSON;
+    
+    
+CREATE OR REPLACE table OUR_FIRST_DB.PUBLIC.JSON_RAW (
+    raw_file variant);
+    
+COPY INTO OUR_FIRST_DB.PUBLIC.JSON_RAW
+    FROM @MANAGE_DB.EXTERNAL_STAGES.JSONSTAGE
+    file_format= MANAGE_DB.FILE_FORMATS.JSONFORMAT
+    files = ('HR_data.json');
+    
+   
+SELECT * FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+// Second step: Parse & Analyse Raw JSON 
+
+   // Selecting attribute/column
+
+SELECT RAW_FILE:city FROM OUR_FIRST_DB.PUBLIC.JSON_RAW
+
+SELECT $1:first_name FROM OUR_FIRST_DB.PUBLIC.JSON_RAW
+
+   // Selecting attribute/column - formattted
+
+SELECT RAW_FILE:first_name::string as first_name  FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+SELECT RAW_FILE:id::int as id  FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
 SELECT 
- raw_author:AUTHOR_UID
-,raw_author:FIRST_NAME::STRING as FIRST_NAME
-,raw_author:MIDDLE_NAME::STRING as MIDDLE_NAME
-,raw_author:LAST_NAME::STRING as LAST_NAME
-FROM AUTHOR_INGEST_JSON;
+    RAW_FILE:id::int as id,  
+    RAW_FILE:first_name::STRING as first_name,
+    RAW_FILE:last_name::STRING as last_name,
+    RAW_FILE:gender::STRING as gender
+
+FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+   // Handling nested data
+   
+SELECT RAW_FILE:job as job  FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+SELECT 
+      RAW_FILE:job.salary::INT as salary
+FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+SELECT 
+    RAW_FILE:first_name::STRING as first_name,
+    RAW_FILE:job.salary::INT as salary,
+    RAW_FILE:job.title::STRING as title
+FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+// Handling arreys
+
+SELECT
+    RAW_FILE:prev_company as prev_company
+FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+SELECT
+    RAW_FILE:prev_company[1]::STRING as prev_company
+FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+SELECT
+    ARRAY_SIZE(RAW_FILE:prev_company) as prev_company
+FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+SELECT 
+    RAW_FILE:id::int as id,  
+    RAW_FILE:first_name::STRING as first_name,
+    RAW_FILE:prev_company[0]::STRING as prev_company
+FROM OUR_FIRST_DB.PUBLIC.JSON_RAW
+UNION ALL 
+SELECT 
+    RAW_FILE:id::int as id,  
+    RAW_FILE:first_name::STRING as first_name,
+    RAW_FILE:prev_company[1]::STRING as prev_company
+FROM OUR_FIRST_DB.PUBLIC.JSON_RAW
+ORDER BY id;
+
+
+
+
+SELECT 
+    RAW_FILE:spoken_languages as spoken_languages
+FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+SELECT * FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+
+SELECT 
+     array_size(RAW_FILE:spoken_languages) as spoken_languages
+FROM OUR_FIRST_DB.PUBLIC.JSON_RAW
+
+
+SELECT 
+     RAW_FILE:first_name::STRING as first_name,
+     array_size(RAW_FILE:spoken_languages) as spoken_languages
+FROM OUR_FIRST_DB.PUBLIC.JSON_RAW
+
+
+
+SELECT 
+    RAW_FILE:spoken_languages[0] as First_language
+FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+
+SELECT 
+    RAW_FILE:first_name::STRING as first_name,
+    RAW_FILE:spoken_languages[0] as First_language
+FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+
+SELECT 
+    RAW_FILE:first_name::STRING as First_name,
+    RAW_FILE:spoken_languages[0].language::STRING as First_language,
+    RAW_FILE:spoken_languages[0].level::STRING as Level_spoken
+FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+SELECT 
+    RAW_FILE:id::int as id,
+    RAW_FILE:first_name::STRING as First_name,
+    RAW_FILE:spoken_languages[0].language::STRING as First_language,
+    RAW_FILE:spoken_languages[0].level::STRING as Level_spoken
+FROM OUR_FIRST_DB.PUBLIC.JSON_RAW
+UNION ALL 
+SELECT 
+    RAW_FILE:id::int as id,
+    RAW_FILE:first_name::STRING as First_name,
+    RAW_FILE:spoken_languages[1].language::STRING as First_language,
+    RAW_FILE:spoken_languages[1].level::STRING as Level_spoken
+FROM OUR_FIRST_DB.PUBLIC.JSON_RAW
+UNION ALL 
+SELECT 
+    RAW_FILE:id::int as id,
+    RAW_FILE:first_name::STRING as First_name,
+    RAW_FILE:spoken_languages[2].language::STRING as First_language,
+    RAW_FILE:spoken_languages[2].level::STRING as Level_spoken
+FROM OUR_FIRST_DB.PUBLIC.JSON_RAW
+ORDER BY ID;
+
+select
+      RAW_FILE:first_name::STRING as First_name,
+    f.value:language::STRING as First_language,
+   f.value:level::STRING as Level_spoken
+from OUR_FIRST_DB.PUBLIC.JSON_RAW, table(flatten(RAW_FILE:spoken_languages)) f;
+
+// Option 1: CREATE TABLE AS
+
+CREATE OR REPLACE TABLE Languages AS
+select
+      RAW_FILE:first_name::STRING as First_name,
+    f.value:language::STRING as First_language,
+   f.value:level::STRING as Level_spoken
+from OUR_FIRST_DB.PUBLIC.JSON_RAW, table(flatten(RAW_FILE:spoken_languages)) f;
+
+SELECT * FROM Languages;
+
+truncate table languages;
+
+// Option 2: INSERT INTO
+
+INSERT INTO Languages
+select
+      RAW_FILE:first_name::STRING as First_name,
+    f.value:language::STRING as First_language,
+   f.value:level::STRING as Level_spoken
+from OUR_FIRST_DB.PUBLIC.JSON_RAW, table(flatten(RAW_FILE:spoken_languages)) f;
+
+
+SELECT * FROM Languages;
+
